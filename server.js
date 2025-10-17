0dd002e9a7004296e6950484d7e80e06ce3de11b
tamoor <ahmadtamoor95@gmail.com>
2025-10-17 18:11:22 +0500

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
